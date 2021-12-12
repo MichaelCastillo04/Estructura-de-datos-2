290beb91ae6850ff2436e79c80702ca20c6401d6
MichaelCastillo04 <91497128+MichaelCastillo04@users.noreply.github.com>
2021-12-11 21:15:59 -0500

    Parcial 2

---
+++ Parcial 2/Parcial2.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+import requests
+import decimal
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class worldBank(Resource):
+    def get(self):
+        data = pd.read_csv('WorldbankD.csv',
+                            error_bad_lines=False, engine='python')
+        data = data.to_dict()
+        return {'data': data}, 200
+
+    pass
+
+
+api.add_resource(worldBank, '/worldbank')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/datos')
+def datos():
+    data = requests.get("http://127.0.0.1:5000/worldbank").json()
+    country = (data["data"]["Data Source"].get("2"))
+    totalData = len(data["data"])
+    i = 0
+    lastPorcent = 0
+    year = 0
+    for db in data["data"]:
+        i += 1
+        current = db
+        if(totalData - 1 == i):
+            lastPorcent = data["data"][current]["2"]
+            year = current
+
+    return render_template("datos.html", pais=country, data=lastPorcent, year=year)
+
+
+@app.route('/prome')
+def prome():
+    data = requests.get("http://127.0.0.1:5000/worldbank").json()
+    totalData = len(data["data"])
+    i = 0
+    totalPorcent = 0
+    for db in data["data"]:
+        i += 1
+        current = db
+        if(i > 4):
+            totalPorcent += data["data"][current]["2"]
+    porcent = decimal.Decimal(totalPorcent / (totalData - 4))
+    print(int(totalData))
+
+    return render_template("prome.html", porcent=round(porcent, 2))
+
+
+if __name__ == '__main__':
+    app.run() 
